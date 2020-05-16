22af25bca9dcb184db5f299595388297b6be4d88
akdeaso <akdeaso@gmail.com>
2020-05-16 12:34:47 +0700

    add files

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on('ready', () => {
+  client.user.setStatus('available')
+  client.user.setPresence({
+      game: {
+          name: 'https://shabadabada.home.blog/',
+      }
+  });
+});
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!")
+  }
+})
+client.login("NzEwOTc3MDU5Mzg3ODY3MjE4.Xr8Tdg.qOOYtXiQopJKBbQ-Uirr-5npcZs")
